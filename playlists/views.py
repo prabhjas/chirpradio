fac4b5ac8b9f1982ba0c9bb1e9c362b9a80ea05a
kumar <kumar.mcmillan@gmail.com>
2010-02-13 23:32:09 -0600

    Bugfix for editing spot copy: the spot dropdown box is now properly selected

---
+++ playlists/views.py
@@ -0,0 +1,290 @@
+###
+### Copyright 2009 The Chicago Independent Radio Project
+### All Rights Reserved.
+###
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+###
+
+"""Views for DJ Playlists."""
+import logging
+from datetime import datetime, timedelta
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import loader, RequestContext
+
+from google.appengine.api.datastore_errors import BadKeyError
+
+from auth.decorators import require_role
+import auth
+from auth import roles
+from playlists.forms import PlaylistTrackForm, PlaylistReportForm
+from playlists.models import PlaylistTrack, PlaylistEvent, PlaylistBreak, ChirpBroadcast
+from playlists.tasks import playlist_event_listeners
+from common.utilities import as_encoded_str
+from common.autoretry import AutoRetry
+
+
+common_context = {
+    'title': 'CHIRPradio.org DJ Playlist Tracker'
+}
+
+class PlaylistEventView(object):
+    """UI wrapper around playlist event data object
+    with extra attributes for use in templates.
+
+    Extra attributes available on view objects:
+
+    **is_break**
+    True if this event is a song break
+
+    **is_new**
+    True if this event is a song played before the last break.
+    Note that this attribute is controlled by iter_playlist_events_for_view()
+
+    """
+
+    def __init__(self, playlist_event):
+        self.playlist_event = playlist_event
+        self.is_break = type(self.playlist_event) is PlaylistBreak
+        self.is_new = False
+
+    def __getattr__(self, key):
+        return getattr(self.playlist_event, key)
+
+def iter_playlist_events_for_view(query):
+    """Iterate a query of playlist event objects.
+
+    returns a generator to produce PlaylistEventView() objects
+    which contain some extra attributes for the view.
+    """
+    first_break = False
+    for playlist_event in AutoRetry(query):
+        pl_view = PlaylistEventView(playlist_event)
+        if pl_view.is_break:
+            first_break = True
+        if not first_break:
+            pl_view.is_new = True
+        yield pl_view
+
+
+
+def get_vars(request):
+    current_user = auth.get_current_user(request)
+
+    if request.method == 'POST':
+        form = PlaylistTrackForm(data=request.POST)
+    else:
+        form = PlaylistTrackForm()
+    form.current_user = current_user
+    form.playlist = ChirpBroadcast()
+
+    vars = {
+        'form': form,
+        'playlist': form.playlist
+    }
+    vars.update(common_context)
+    return vars
+
+
+def get_playlist_history(playlist):
+    pl = PlaylistEvent.all().filter('playlist =', playlist)
+    pl = pl.filter('established >=', datetime.now() - timedelta(hours=3))
+    pl = pl.order('-established')
+    return list(iter_playlist_events_for_view(pl))
+
+@require_role(roles.DJ)
+def landing_page(request):
+    vars = get_vars(request)
+
+    # load the playlist history
+    vars['playlist_events'] = get_playlist_history(vars['playlist'])
+
+    return render_to_response('playlists/landing_page.html', vars,
+            context_instance=RequestContext(request))
+
+
+@require_role(roles.DJ)
+def create_event(request):
+    vars = get_vars(request)
+
+    if request.method == 'POST':
+
+        # special case...
+        if request.POST.get('submit') == 'Add Break':
+            b = PlaylistBreak(playlist=vars['playlist'])
+            b.put()
+            vars['add_break'] = True
+            # errors should not display on add break, reset internal hash
+            vars['form']._errors = {}
+
+        elif vars['form'].is_valid():
+            track = vars['form'].save()
+            playlist_event_listeners.create(track)
+            return HttpResponseRedirect(reverse('playlists_landing_page'))
+
+    vars['playlist_events'] = get_playlist_history(vars['playlist'])
+
+    return render_to_response('playlists/landing_page.html', vars,
+            context_instance=RequestContext(request))
+
+
+@require_role(roles.DJ)
+def delete_event(request, event_key):
+    e = None
+    try:
+        e = AutoRetry(PlaylistEvent).get(event_key)
+    except BadKeyError:
+        pass
+    else:
+        if e and e.selector.key() == auth.get_current_user(request).key():
+            e.delete()
+            playlist_event_listeners.delete(event_key)
+
+    return HttpResponseRedirect(reverse('playlists_landing_page'))
+
+
+
+@require_role(roles.MUSIC_DIRECTOR)
+def report_playlist(request):
+    vars = {}
+
+    # report vars
+    items = None
+    fields = ['from_date', 'to_date', 'album_title', 'artist_name', 'label', 'play_count']
+
+    # default report
+    if request.method == 'GET':
+        to_date = datetime.now().date()
+        from_date = to_date - timedelta(days=7)
+        items = query_group_by_track_key(from_date, to_date)
+
+        # default form
+        form = PlaylistReportForm({'from_date':from_date, 'to_date':to_date})
+
+    # check form data post
+    elif request.method == 'POST':
+
+        # generic search form
+        form = PlaylistReportForm(data=request.POST)
+        if form.is_valid():
+            from_date = form.cleaned_data['from_date']
+            to_date = form.cleaned_data['to_date']
+            
+
+            # special case to download report
+            if request.POST.get('download') == 'Download':
+                fname = "chirp-play-count_%s_%s" % (from_date, to_date)
+                return http_send_csv_file(fname, fields, query_group_by_track_key(from_date, to_date))
+
+            # generate report from date range
+            if request.POST.get('search') == 'Search':
+                items = query_group_by_track_key(from_date, to_date)
+
+    # template vars
+    vars['form'] = form
+
+    if items:
+        vars['fields'] = fields
+        vars['tracks'] = query_group_by_track_key(from_date, to_date)
+
+    return render_to_response('playlists/reports.html', vars,
+            context_instance=RequestContext(request))
+
+def http_send_csv_file(fname, fields, items):
+    import csv
+
+    # dump item using key fields
+    def item2row(i):
+        return [as_encoded_str(i[key], encoding='utf8') for key in fields]
+
+    # use response obj to set filename of downloaded file
+    response = HttpResponse(mimetype='text/csv')
+    # TODO(Kumar) mark encoding as UTF-8?
+    response['Content-Disposition'] = "attachment; filename=%s.csv" % (fname)
+
+    # write data out
+    out = csv.writer(response)
+    out.writerow(fields)
+    for item in items:
+        out.writerow(item2row(item))
+    #
+    return response
+
+# TODO: move following funcs to models
+def filter_tracks_by_date_range(from_date, to_date):
+    playlist = ChirpBroadcast()
+    pl = PlaylistTrack.all().filter('playlist =', playlist)
+    pl = pl.filter('established >=', from_date)
+    pl = pl.filter('established <=', to_date)
+    pl = pl.order('-established')
+    return pl
+
+def query_group_by_track_key(from_date, to_date):
+    ''' app engine Query and GqlQuery do not support SQL group by
+    manually count each record and group them by some unique key
+    '''
+
+    query = filter_tracks_by_date_range(from_date, to_date)
+    fields = ['album_title', 'artist_name', 'label']
+
+    #
+    key_item = 'group_by_key'
+    key_counter = 'play_count'
+
+    # group by key/fields
+    def item_key(item):
+        key_parts = []
+        for key in fields:
+            stub = as_encoded_str(getattr(item, key, ''))
+            if stub is None:
+                # for existing None-type attributes
+                stub = ''
+            stub = stub.lower()
+            key_parts.append(stub)
+        return ','.join(key_parts)
+
+    # dict version of db rec
+    def item2hash(item):
+        d = {}
+        for key in fields:
+            d[key] = getattr(item, key, None)
+
+        # init additional props
+        d[key_counter] = 0
+        d['from_date'] = from_date
+        d['to_date'] = to_date
+
+        return d
+
+    # unique list of tracks with order
+    items = []
+
+    # hash of seen keys
+    seen = {}
+
+    #
+    for item in AutoRetry(query):
+        key = item_key(item)
+
+        if not seen.has_key(key):
+            x = item2hash(item)
+            seen[key] = x
+            items.append(x)
+
+        # inc counter
+        seen[key][key_counter] += 1
+
+    return items
+
---
+++ traffic_log/tests.py
@@ -0,0 +1,396 @@
+###
+### Copyright 2009 The Chicago Independent Radio Project
+### All Rights Reserved.
+###
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+###
+import os
+import time
+import unittest
+import datetime
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase as DjangoTestCase
+from django import http
+from django.test.client import Client
+
+from common.testutil import FormTestCaseHelper
+from common import time_util
+from auth import roles
+from auth.models import User
+from traffic_log import views, models, constants
+
+def clear_data():
+    for x in models.TrafficLogEntry.all():
+        x.delete()
+    for c in models.SpotCopy.all():
+        c.delete()
+    for s in models.Spot.all():
+        s.delete()
+    for c in models.SpotConstraint.all():
+        c.delete()
+
+class TestTrafficLogViews(DjangoTestCase):
+    
+    def setUp(self):
+        self.client.login(email="test@test.com", roles=[roles.DJ])
+    
+    def tearDown(self):
+        clear_data()
+    
+    def test_landing_page_shows_spots(self):
+        user = User(email='test')
+        user.save()
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot_key = spot.put()
+        # assign it to every day of the week at the top of the hour:
+        constraint_keys = views.saveConstraint(dict(hourbucket="0,24", dow_list=range(1,8), slot=0))
+        views.connectConstraintsAndSpot(constraint_keys, spot_key)
+        
+        resp = self.client.get(reverse('traffic_log.index'))
+        context = resp.context[0]
+        spot_map = {}
+        constraint_map = {}
+        for c in context['slotted_spots']:
+            spot_map[c.hour] = list(c.iter_spots())[0]
+            constraint_map[c.hour] = c
+        
+        now = time_util.chicago_now()
+        
+        self.assertEqual(spot_map[now.hour].title, 'Legal ID')
+        self.assertEqual(spot_map[(now + datetime.timedelta(hours=1)).hour].title, 
+                'Legal ID')
+        self.assertEqual(spot_map[(now + datetime.timedelta(hours=2)).hour].title, 
+                'Legal ID')
+        
+        constraint = constraint_map[now.hour]
+        spot = list(constraint.iter_spots())[0]
+        assert constraint.url_to_finish_spot(spot) in resp.content
+
+class TestTrafficLogAdminViews(FormTestCaseHelper, DjangoTestCase):
+    
+    def setUp(self):
+        self.client.login(email="test@test.com", roles=[roles.TRAFFIC_LOG_ADMIN])
+    
+    def tearDown(self):
+        clear_data()
+    
+    def test_create_spot(self):
+        resp = self.client.post(reverse('traffic_log.createSpot'), {
+            'title': 'Legal ID',
+            'type': 'Station ID',
+            'hourbucket': '0,24',
+            'dow_list': [str(d) for d in range(1,8)],
+            'slot': '0'
+        })
+        self.assertNoFormErrors(resp)
+        spot = models.Spot.all().filter("title =", "Legal ID").fetch(1)[0]
+        dow = set()
+        hours = set()
+        constraint_map = {}
+        for constraint in spot.constraints:
+            dow.add(constraint.dow)
+            hours.add(constraint.hour)
+            constraint_map[(constraint.dow, constraint.hour, constraint.slot)] = constraint
+        self.assertEqual(sorted(dow), range(1,8))
+        self.assertEqual(sorted(hours), range(0,24))
+        
+        # check with Sunday 12:00pm
+        self.assertEqual(constraint_map[(1L, 12L, 0L)].url_to_finish_spot(spot), 
+            "/traffic_log/spot-copy/%s/finish?hour=12&dow=1&slot=0" % spot.key())
+            
+        self.assertEqual(constraint_map[(1L, 12L, 0L)].as_query_string(), 
+            "hour=12&dow=1&slot=0")
+    
+    def test_create_spot_copy(self):
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        resp = self.client.post(reverse('traffic_log.createSpotCopy'), {
+            'spot_key': spot.key(),
+            'body': 'You are listening to chirprario.odg',
+            'underwriter': 'pretend this is an underwriter',
+            'expire_on': ''
+        })
+        self.assertNoFormErrors(resp)
+        
+        spot_copy = spot.copy_at_random
+        self.assertEqual(spot_copy.body, 'You are listening to chirprario.odg')
+        self.assertEqual(spot_copy.underwriter, 'pretend this is an underwriter')
+        self.assertEqual(spot_copy.author.email, 'test@test.com')
+    
+    def test_edit_spot_copy(self):
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        author = User(email='test')
+        author.put()
+        spot_copy = models.SpotCopy(
+                        body='First',
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        spot_copy2 = models.SpotCopy(
+                        body='You are listening to chirpradio.org',
+                        spot=spot,
+                        author=author)
+        spot_copy2.put()
+        
+        # now edit the second one:
+        resp = self.client.post(reverse('traffic_log.editSpotCopy', args=(spot_copy2.key(),)), {
+            'spot_key': spot.key(),
+            'body': 'Something else entirely',
+            'underwriter': 'another underwriter',
+            'expire_on': ''
+        })
+        self.assertNoFormErrors(resp)
+        
+        spot_copy = [c for c in spot.all_spot_copy()]
+        self.assertEqual([c.body for c in spot_copy], ['First','Something else entirely'])
+        self.assertEqual([c.underwriter for c in spot_copy], [None, 'another underwriter'])
+        self.assertEqual([c.author.email for c in spot_copy], ['test', 'test@test.com'])
+    
+    def test_make_spot_copy_expire(self):
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        author = User(email='test')
+        author.put()
+        spot_copy = models.SpotCopy(
+                        body='First',
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        
+        resp = self.client.post(reverse('traffic_log.editSpotCopy', args=(spot_copy.key(),)), {
+            'spot_key': spot.key(),
+            'body': 'Something else entirely',
+            'underwriter': 'another underwriter',
+            'expire_on': '2/5/2010' # any date in the past
+        })
+        self.assertNoFormErrors(resp)
+        
+        spot_copy = [c for c in spot.all_spot_copy()]
+        self.assertEqual(spot_copy, [])
+    
+    def test_create_edit_spot_copy_expiry(self):
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        now = time_util.chicago_now() + datetime.timedelta(hours=2)
+        resp = self.client.post(reverse('traffic_log.views.addCopyForSpot', args=(spot.key(),)), {
+            'spot_key': spot.key(),
+            'body': 'You are listening to chirprario.odg',
+            'underwriter': 'pretend this is an underwriter',
+            'expire_on': now.strftime("%m/%d/%Y %H:%M:%S") # no timezone info
+        })
+        self.assertNoFormErrors(resp)
+        
+        spot_copy = spot.copy_at_random
+        converted_expire_on = time_util.convert_utc_to_chicago(spot_copy.expire_on)
+        self.assertEqual(converted_expire_on.timetuple(), now.timetuple())
+        
+        resp = self.client.post(reverse('traffic_log.editSpotCopy', args=(spot_copy.key(),)), {
+            'spot_key': spot.key(),
+            'body': 'You are listening to chirprario.odg',
+            'underwriter': 'pretend this is an underwriter',
+            'expire_on': spot_copy.expire_on.strftime("%m/%d/%Y %H:%M:%S") # no timezone info
+        })
+        self.assertNoFormErrors(resp)
+        
+        spot_copy = spot.copy_at_random
+        converted_expire_on = time_util.convert_utc_to_chicago(spot_copy.expire_on)
+        self.assertEqual(converted_expire_on.timetuple(), now.timetuple())
+    
+    def test_delete_spot_copy(self):
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        author = User(email='test')
+        author.put()
+        spot_copy = models.SpotCopy(
+                        body='First',
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        
+        # now edit the second one:
+        resp = self.client.get(reverse('traffic_log.deleteSpotCopy', args=(spot_copy.key(),)))
+        
+        self.assertEqual([c for c in spot.all_spot_copy()], [])
+
+class TestObjects(DjangoTestCase):
+    
+    def test_spot_copy(self):
+        author = User(email='test')
+        author.save()
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        self.assertEqual(spot.get_add_copy_url(), 
+                            reverse('traffic_log.views.addCopyForSpot', args=(spot.key(),)))
+        
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        spot_copy = models.SpotCopy(
+                        body=(
+                            'You are now and forever listening to a killer '
+                            'radio station called chirpradio.org'),
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        
+        self.assertEqual(str(spot_copy), "You are now and forever listening to a killer radio...")
+        self.assertEqual(spot_copy.get_edit_url(), 
+                            reverse('traffic_log.editSpotCopy', args=(spot_copy.key(),)))
+        self.assertEqual(spot_copy.get_delete_url(), 
+                            reverse('traffic_log.deleteSpotCopy', args=(spot_copy.key(),)))
+
+class TestTrafficLogDJViews(FormTestCaseHelper, DjangoTestCase):
+    
+    def setUp(self):
+        self.client.login(email="dj-test@test.com", roles=[roles.DJ])
+    
+    def tearDown(self):
+        clear_data()
+    
+    def test_view_spot_for_reading(self):
+        author = User(email='test')
+        author.save()
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID')
+        spot.put()
+        constraint = models.SpotConstraint(dow=1, hour=0, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        spot_copy = models.SpotCopy(
+                        body='You are listening to chirpradio.org',
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        
+        resp = self.client.get(reverse('traffic_log.spotTextForReading', args=(spot.key(),)), {
+            'hour': constraint.hour,
+            'dow': constraint.dow,
+            'slot': constraint.slot
+        })
+        context = resp.context
+        self.assertEqual(context['spot_copy'].body, 'You are listening to chirpradio.org')
+        self.assertEqual(context['url_to_finish_spot'], 
+            "/traffic_log/spot-copy/%s/finish?hour=0&dow=1&slot=0" % spot_copy.key())
+
+    def test_finish_spot(self):
+        self.assertEqual(list(models.TrafficLogEntry.all().fetch(5)), [])
+        
+        author = User(email='test')
+        author.save()
+        spot = models.Spot(
+                        title='Legal ID',
+                        type='Station ID', 
+                        author=author)
+        spot.put()
+        
+        # make a constraint closest to now:
+        now = time_util.chicago_now()
+        today = now.date()
+        current_hour = now.hour
+        constraint = models.SpotConstraint(
+            dow=today.isoweekday(), hour=current_hour, slot=0, spots=[spot.key()])
+        constraint.put()
+        
+        spot_copy = models.SpotCopy(
+                        body='You are listening to chirpradio.org',
+                        spot=spot,
+                        author=author)
+        spot_copy.put()
+        
+        resp = self.client.get(reverse('traffic_log.index'))
+        # unfinished spot should have been marked in static HTML:
+        assert '<tr class="new">' in resp.content
+        
+        resp = self.client.get(reverse('traffic_log.finishReadingSpotCopy', args=(spot_copy.key(),)), {
+            'hour': constraint.hour,
+            'dow': constraint.dow,
+            'slot': constraint.slot
+        })
+        logged = models.TrafficLogEntry.all().fetch(1)[0]
+        self.assertEqual(logged.reader.email, "dj-test@test.com")
+        self.assertEqual(logged.readtime.timetuple()[0:5], datetime.datetime.now().timetuple()[0:5])
+        self.assertEqual(logged.log_date, time_util.chicago_now().date())
+        self.assertEqual(logged.spot.key(), spot.key())
+        self.assertEqual(logged.spot_copy.key(), spot_copy.key())
+        self.assertEqual(logged.scheduled.key(), constraint.key())
+        self.assertEqual(logged.hour, constraint.hour)
+        self.assertEqual(logged.dow, constraint.dow)
+        
+        resp = self.client.get(reverse('traffic_log.index'))
+        # finished spot should have been marked in static HTML:
+        assert '<tr class="finished">' in resp.content
+    
+class TrafficLogTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.admin_u = User(email='test')
+        self.admin_u.roles.append(roles.TRAFFIC_LOG_ADMIN)
+    
+        self.dj_u = User(email='test2')
+        self.dj_u.roles.append(roles.DJ)
+
+        self.user_u = User(email='test3')
+
+    def test_trafficlog_roles(self):
+        self.assertTrue(self.admin_u.is_traffic_log_admin)
+        self.assertTrue(self.dj_u.is_dj)
+        self.assertTrue(self.user_u.is_active)
+
+
+    def test_spot_constraint_assign(self):
+        user = User(email='test')
+        user.save()
+        spot_key = models.Spot(title='test',body='body',type='Live Read Promo', author=user).put()
+        constraint_key = models.SpotConstraint(dow=1,hour=1,slot=0).put()
+        views.connectConstraintsAndSpot([constraint_key], spot_key)
+        self.assertEqual(models.Spot.get(spot_key).constraints.count(), 1)
+
+    def test_traffic_log_generate(self):
+        pass
+    
+    def test_spot_constraint_delete(self):
+        pass
+
+
