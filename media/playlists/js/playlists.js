d1e5b7c7a2af650d5a93bde834281bd948e7624e
mike <mike@chirpradio.org>
2010-03-02 17:38:16 -0600

    Reverted back to backup files because something got seriously fucked

---
+++ media/playlists/js/playlists.js
@@ -0,0 +1,86 @@
+// requires: chirp/chirp.js
+//           jquery.autocomplete/jquery.autocomplete.js
+
+$(document).ready(function() {
+    
+    var default_opt = {
+        selectFirst: false,
+        minChars: 3,
+        delay: 400,
+        maxItemsToShow: 15,
+        matchContains: true // tells the cache to do substring matches 
+                            // (necessary when searching "eno" and the 
+                            // result is "Eno, Brian")
+    };
+    
+    $("#id_artist").autocomplete("/djdb/artist/search.txt", 
+        $.extend({
+            onItemSelect: function(li) {
+                var entity_key = li.extra[0];
+                var song = $("#id_song").get(0);
+                song.autocompleter.setExtraParams({artist_key: entity_key});
+                song.autocompleter.flushCache();
+                $("#id_artist_key").attr("value", entity_key);
+                $("#id_artist").focus();
+            }
+        }, default_opt));
+    
+    $("#id_album").autocomplete("/djdb/album/search.txt", 
+        $.extend({
+            onItemSelect: function(li) {
+                var entity_key = li.extra[0];
+                $("#id_album_key").attr("value", entity_key);
+                $("#id_album").focus();
+            }
+        }, default_opt));
+    
+    $("#id_song").autocomplete("/djdb/track/search.txt", 
+        $.extend({
+            onItemSelect: function(li) {
+                var entity_key = li.extra[0];
+                $("#id_song_key").attr("value", entity_key);
+                $("#id_song").focus();
+            }
+        }, default_opt));
+    
+    // be sure that freeform entry always clears out any 
+    // previously auto-completed keys :
+    
+    $("#id_artist").change(function() {
+        var song = $("#id_song").get(0);
+        song.autocompleter.setExtraParams({artist_key: ""});
+        song.autocompleter.flushCache();
+        $("#id_artist_key").attr("value", "");
+    });
+    $("#id_album").change(function() {
+        $("#id_album_key").attr("value", "");
+    });
+    $("#id_song").change(function() {
+        $("#id_song_key").attr("value", "");
+    });
+    
+    $('#lookup-on-musicbrainz').click(function(e) {
+        var url = 'http://musicbrainz.org/search/textsearch.html';
+        var qs = 'limit=25&adv=on&handlearguments=1';
+        var artist = $('#id_artist').val();
+        var album = $('#id_album').val();
+        var song = $('#id_song').val();
+        if ( !artist ) {
+            e.preventDefault();
+            return;
+        }
+        if (artist && album) {
+            qs += '&type=release';
+            qs += '&query=' + escape('"' + album + '" AND artist:' + artist);
+        } else if (artist && song) {
+            qs += '&type=track';
+            qs += '&query=' + escape('"' + song + '" AND artist:' + artist);
+        } else {
+            qs += '&type=artist';
+            qs += '&query=' + escape(artist);
+        }
+        // console.log(qs);
+        
+        this.href = url + '?' + qs;
+    });
+});
---
+++ media/traffic_log/js/trafficlog.js
@@ -0,0 +1,133 @@
+// requires: chirp/chirp.js
+//           
+
+chirp.traffic_log = {};
+
+$(document).ready(function() {
+    
+    var ns = chirp.traffic_log;
+    
+    ns.handle_finish_spot = function(anchor, tr) {
+        var url = $(anchor).attr("href");
+        $.ajax({
+            url: url,
+            success: function(data, textStatus) {
+                tr.removeClass("new");
+                tr.addClass("finished");
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Whoops, there was an error on the server. An email has been sent to the admins so sit tight or try again.");
+            }
+        });
+    };
+    
+    $("#refresh-button").click(function() {
+        window.location = "/traffic_log/?t=" + Math.random().toString();
+    });
+    
+    // $(document).bind('beforeReveal.facebox', function() {
+    //     $('#facebox table').width(700);
+    // });
+    
+    $('#modal-container').jqm({
+      ajax: '@href',
+      overlay: 75,
+      trigger: '.show-text-for-reading'
+    });
+    
+//    $(".show-text-for-reading").click(function(e) {
+//        e.preventDefault();
+//        var $this = $(this);
+//        var url = $(this).attr("href");
+//        var tr = $(this).parent().parent(); // a->td->tr
+
+//        $.facebox(function() {
+//            $.ajax({
+//                url: url,
+//                success: function(data, textStatus) {
+//                    $.facebox(data);
+//                    var onclick = function(e) {
+//                        e.preventDefault();
+//                        var anchor = this;
+//                        ns.handle_finish_spot(anchor, tr);
+//                        $(document).trigger('close.facebox');
+//                        $(this).unbind("click", onclick);
+//                    };
+//                    $('.finish-spot-after-reading').click(onclick);
+//                },
+//                error: function (XMLHttpRequest, textStatus, errorThrown) {
+//                    alert("Whoops, there was an error on the server. An email has been sent to the admins so sit tight or try again.");
+//                    $(document).trigger('close.facebox');
+//                }
+//            });
+//        });
+        
+//    });
+    
+    $(".finish-spot").click(function(e) {
+        e.preventDefault();
+        var anchor = this;
+        var tr = $(anchor).parent().parent(); // a->td->tr
+        ns.handle_finish_spot(anchor, tr);
+        $(this).remove();
+    });
+    
+    // sigh. this is all necessary to override the image paths:
+//    $.extend($.facebox.settings, {
+//        loadingImage : '/media/common/js/jquery-facebox/loading.gif',
+//        closeImage   : '/media/common/js/jquery-facebox/closelabel.gif',
+//        faceboxHtml  : '\
+//    <div id="facebox" style="display:none;"> \
+//      <div class="popup"> \
+//        <table> \
+//          <tbody> \
+//            <tr> \
+//              <td class="tl"/><td class="b tb"/><td class="tr"/> \
+//            </tr> \
+//            <tr> \
+//              <td class="b"/> \
+//              <td class="body"> \
+//                <div class="content"> \
+//                </div> \
+//                <div class="footer"> \
+//                  <a href="#" class="close"> \
+//                    <img src="/media/common/js/jquery-facebox/closelabel.gif" title="close" class="close_image" /> \
+//                  </a> \
+//                </div> \
+//              </td> \
+//              <td class="b"/> \
+//            </tr> \
+//            <tr> \
+//              <td class="bl"/><td class="b"/><td class="br"/> \
+//            </tr> \
+//          </tbody> \
+//        </table> \
+//      </div> \
+//    </div>'
+//        }
+//    );
+});
+
+
+// Kumar: might use this timer to display a notice but not a redirect
+// because a DJ might be reading a spot when the timeout occurs
+
+/*var start_timer = function() {
+    var time = new Date();
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    if ( seconds == 0 && minutes == 0 ) {
+        window.location.replace( unescape( window.location.pathname ) ); 
+    } else {
+		if( minutes < 10 ){
+			minutes = '0' + minutes;
+		} 
+		if ( seconds < 10 ){
+			seconds = '0' + seconds;
+		}
+
+        self.status = hour + ":" + minutes +":" + seconds;
+        self.setTimeout("start_timer()", 60)
+    }
+}*/
