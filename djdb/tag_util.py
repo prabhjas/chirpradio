5cdd314d3c4b9f79cd472d6fb7614e72f8f9478d
kumar <kumar.mcmillan@gmail.com>
2011-02-14 09:45:08 -0600

    Catches LastFM exceptions now

---
+++ djdb/tag_util.py
@@ -0,0 +1,137 @@
+###
+### Copyright 2009 The Chicago Independent Radio Project
+### All Rights Reserved.
+###
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+###
+
+from datetime import datetime, timedelta
+
+from google.appengine.ext import db
+
+from djdb import models
+from djdb import search
+from common.autoretry import AutoRetry
+
+
+def modify_tags_and_save(user, obj, to_add, to_remove):
+    """Modify the set of tags attached to an object, and save to the datastore.
+
+    Args:
+      user: The User object of the person responsible for this change
+        to the tags.
+      obj: The object (either an Album or a Track) containing the tags.
+      to_add: A sequence of tags to add to the object.
+      to_remove: A sequence of tags to remove from the object.
+
+    Returns:
+      True if a modified version of the object was saved, or False if
+      no changes were necessary.
+    """
+    to_add = list(set(to_add).difference(obj.current_tags))
+    to_remove = list(set(to_remove).intersection(obj.current_tags))
+    if not (to_add or to_remove):
+        return False
+    obj.current_tags = list(
+        set(obj.current_tags).union(to_add).difference(to_remove))
+    tag_edit = models.TagEdit(parent=obj, subject=obj, author=user,
+                              added=to_add, removed=to_remove)
+    # The two objects are in the same entity group, so saving them
+    # is an all-or-nothing operation.
+    AutoRetry(db).save([obj, tag_edit])
+    
+    # Update search indexer.
+    idx = search.Indexer(obj.parent_key())
+    for tag in to_remove:
+        idx.remove_key(obj.key(), 'tag', tag)
+    for tag in to_add:
+        idx.add_key(obj.key(), 'tag', tag)
+    idx.save()
+    
+    return True
+
+
+def set_tags_and_save(user, obj, list_of_new_tags):
+    """Assign a list of tags to an object, and save to the datastore.
+
+    Args:
+      user: The User object of the person responsible for this change
+        to the tags.
+      obj: The object (either an Album or a Track) containing the tags.
+      list_of_new_tags: A sequence containing the tags to assign to the
+        object.
+
+    Returns:
+      True if a modified version of the object was saved, or False if
+      no changes were necessary.
+    """
+    to_add = list(set(list_of_new_tags).difference(obj.current_tags))
+    to_remove = list(set(obj.current_tags).difference(list_of_new_tags))
+    return modify_tags_and_save(user, obj, to_add, to_remove)
+
+
+def add_tag_and_save(user, obj, new_tag, insert=False):
+    """Adds a single tag to an object, and save to the datastore.
+
+    Args:
+      user: The User object of the person responsible for this change
+        to the tags.
+      obj: The object (either an Album or a Track) containing the tags.
+      new_tag: A single item to add to the object's list of tags.
+      insert: Whether to insert a new tag entity into the datastore.
+
+    Returns:
+      True if a modified version of the object was saved, or False if
+      no changes were necessary.
+    """
+    if insert and AutoRetry(models.Tag.all().filter("name =", new_tag)).count() == 0:
+        tag = models.Tag(name=new_tag)
+        AutoRetry(tag).put()
+        
+    return modify_tags_and_save(user, obj, [new_tag], [])
+
+
+def remove_tag_and_save(user, obj, tag_to_remove):
+    """Removes a single tag from an object, and save to the datastore.
+
+    Args:
+      user: The User object of the person responsible for this change
+        to the tags.
+      obj: The object (either an Album or a Track) containing the tags.
+      tag_to_remove: A single item to remove from the object's list of tags.
+
+    Returns:
+      True if a modified version of the object was saved, or False if
+      no changes were necessary.
+    """
+    return modify_tags_and_save(user, obj, [], [tag_to_remove])
+
+    
+def fetch_recent(max_num_returned=10, days=None, start_dt=None):
+    if days is not None:
+        if start_dt:
+            end_dt = start_dt - timedelta(days=days)
+        else:
+            end_dt = datetime.now() - timedelta(days=days)
+    else:
+        end_dt = None
+
+    q = models.TagEdit.all().order('-timestamp')
+    if start_dt:
+        q.filter('timestamp <', start_dt)
+    if end_dt:
+        q.filter('timestamp >=', end_dt)
+    tag_edits = q.fetch(max_num_returned);
+
+    return tag_edits
+    
---
+++ playlists/models.py
@@ -0,0 +1,228 @@
+###
+### Copyright 2009 The Chicago Independent Radio Project
+### All Rights Reserved.
+###
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+###
+
+"""Datastore model for DJ Playlists."""
+
+from google.appengine.ext.db import polymodel
+from google.appengine.api import memcache
+from google.appengine.ext import db
+from google.appengine.api.datastore_types import Key
+
+from auth.models import User
+import auth
+from djdb.models import Artist, Album, Track
+from common import time_util
+from common.autoretry import AutoRetry
+
+class Playlist(polymodel.PolyModel):
+    """A playlist of songs.
+    """
+    # The date this playlist was established 
+    # (automatically set to now upon creation)
+    established = db.DateTimeProperty(auto_now_add=True)
+    # The date this playlist was last modified (automatically set to now)
+    modified = db.DateTimeProperty(auto_now=True)
+    
+    @property
+    def established_display(self):
+        return time_util.convert_utc_to_chicago(self.established)
+        
+    @property
+    def modified_display(self):
+        return time_util.convert_utc_to_chicago(self.modified)
+    
+    @property
+    def recent_tracks(self):
+        """Generates a list of recently played tracks in this playlist"""
+        q = PlaylistTrack.all().filter('playlist =', self).order('-established')
+        for track in q.fetch(1000):
+            yield track
+    
+    @property
+    def recent_events(self):
+        """Generates a list of recent events in this playlist.
+        
+        This is like self.recent_tracks but also includes breaks.
+        """
+        q = PlaylistEvent.all().filter('playlist =', self).order('-established')
+        for event in q.fetch(1000):
+            yield event
+
+class DJPlaylist(Playlist):
+    """A playlist created by a DJ.
+    
+    This might be in preparation for a show or just for organizational purposes.
+    """
+    # A name to identify this playlist by
+    name = db.StringProperty(required=True)
+    # DJ user who created the playlist, if relevant
+    created_by_dj = db.ReferenceProperty(User, required=True)
+    # Number of tracks contained in this playlist.
+    # TODO(kumar) this is not currently used.
+    track_count = db.IntegerProperty(default=0, required=True)
+    
+    def validate(self):
+        """Validate this instance before putting it to the datastore."""
+        if not self.created_by_dj.is_dj:
+            raise ValueError("User %r must be a DJ (user is: %r)" % (
+                                self.created_by_dj, self.created_by_dj.roles))
+    
+    def put(self, *args, **kwargs):
+        self.validate()
+        super(DJPlaylist, self).put(*args, **kwargs)
+
+class BroadcastPlaylist(Playlist):
+    """A continuous playlist for a live broadcast."""
+    # The name of the broadcast channel
+    channel = db.StringProperty(required=True)
+
+def ChirpBroadcast():
+    """The continuous CHIRP broadcast"""
+    
+    # There is only one persistant live-stream stream.
+    # If it doesn't exist, create it once for all time
+    
+    query = BroadcastPlaylist.all().filter('channel =', 'CHIRP')
+    if AutoRetry(query).count(1):
+        playlist = AutoRetry(query)[0]
+    else:
+        playlist = BroadcastPlaylist(channel='CHIRP')
+        AutoRetry(playlist).put()
+    
+    return playlist
+    
+class PlaylistEvent(polymodel.PolyModel):
+    """An event that occurs in a Playlist."""
+    # The playlist this event belongs to
+    playlist = db.ReferenceProperty(Playlist, required=True)
+    # The date this playlist event was established 
+    # (automatically set to now upon creation)
+    established = db.DateTimeProperty(auto_now_add=True)
+    # The date this playlist event was last modified (automatically set to now)
+    modified = db.DateTimeProperty(auto_now=True)
+    
+    @property
+    def established_display(self):
+        return time_util.convert_utc_to_chicago(self.established)
+        
+    @property
+    def modified_display(self):
+        return time_util.convert_utc_to_chicago(self.modified)
+
+class PlaylistBreak(PlaylistEvent):
+    """A break in a playlist.
+    
+    Typically this is what a DJ will use to indicate that it's time 
+    to talk over the air.  The DJ would glance at the playlist and read 
+    aloud the four or five songs that were played since the last break
+    """
+    # this doesn't have any special fields
+
+class PlaylistTrack(PlaylistEvent):
+    """A track in a Playlist."""
+    # DJ user who selected this track.
+    selector = db.ReferenceProperty(User, required=True)
+    # Artist name if this is a freeform entry
+    freeform_artist_name = db.StringProperty(required=False)
+    # Reference to artist from CHIRP digital library (if exists in library)
+    artist = db.ReferenceProperty(Artist, required=False)
+    # Track title if this is a freeform entry
+    freeform_track_title = db.StringProperty(required=False)
+    # Reference to track (mp3 file) from CHIRP digital library (if exists in library)
+    track = db.ReferenceProperty(Track, required=False)
+    # The order at which this track appears in the playlist
+    track_number = db.IntegerProperty(required=True, default=1)
+    # Album title if this is a freeform entry
+    freeform_album_title = db.StringProperty(required=False)
+    # Reference to album from CHIRP digital library (if exists in library)
+    album = db.ReferenceProperty(Album, required=False)
+    # Label if this is a freeform entry
+    freeform_label = db.StringProperty(required=False)
+    # Notes about this track
+    notes = db.TextProperty(required=False)
+    
+    @property
+    def artist_name(self):
+        # validate() should enforce that one of these is available:
+        if self.artist:
+            return self.artist.name
+        else:
+            return self.freeform_artist_name
+    
+    @property
+    def track_title(self):
+        # validate() should enforce that one of these is available:
+        if self.track:
+            return self.track.title
+        else:
+            return self.freeform_track_title
+    
+    @property
+    def album_title(self):
+        if self.album:
+            return self.album.title
+        elif self.freeform_album_title:
+            return self.freeform_album_title
+        else:
+            return None
+    
+    @property
+    def album_title_display(self):
+        txt = self.album_title
+        if txt:
+            return txt
+        else:
+            return u"[Unknown Album]"
+    
+    @property
+    def label(self):
+        # TODO(kumar) when a Label entity exists in the 
+        # the DJDB then we should provide a way to fetch by reference
+        if self.freeform_label:
+            return self.freeform_label
+        else:
+            return None
+    
+    @property
+    def label_display(self):
+        txt = self.label
+        if txt:
+            return txt
+        else:
+            return u"[Unknown Label]"
+    
+    def validate(self):
+        """Validate this instance before putting it to the datastore.
+        
+        A track must have at least artist name and track title
+        """
+        if not self.track_title and not self.track:
+            raise ValueError("Must set either a track_title or reference a track")
+        if not self.artist_name and not self.artist:
+            raise ValueError("Must set either an artist_name or reference an artist")
+        if not self.selector.is_dj:
+            raise ValueError("User %r must be a DJ (user is: %r)" % (
+                                self.selector, self.selector.roles))
+    
+    def put(self, *args, **kwargs):
+        self.validate()
+        super(PlaylistTrack, self).put(*args, **kwargs)
+        memcache.delete('api.current_track')
+    
+    def save(self, *args, **kwargs):
+        return self.put(*args, **kwargs)
+
